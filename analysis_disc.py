e38e005d4d501b7832e7eaaf2490d09b300367e4
Daniel Mentiplay <d.mentiplay@gmail.com>
2019-01-23 09:21:12 +1100

    analysis_disc.py: calculate dust surface density
    Former-commit-id: 1b2cd594d89aa6f849ab37949c163cb834c34684

--- analysis_disc.py
+++ analysis_disc.py
@@ -17,18 +17,30 @@ dump = Dump('disc_00000.ascii')
 
 #--- Units
 
-udist = dump.units['dist']
-utime = dump.units['time']
-umass = dump.units['mass']
+unitDist = dump.units['dist']
+unitTime = dump.units['time']
+unitMass = dump.units['mass']
+
+unitDens = unitMass / unitDist**3
+unitSurfaceDens = unitMass / unitDist**2
 
 #--- Particle properties
 
-massGasParticle = dump.massParticles['gas']
-position        = dump.position['gas']
-velocity        = dump.velocity['gas']
-momentum        = massGasParticle * velocity
-angularMomentum = np.cross(position, momentum)
-smoothingLength = dump.smoothingLength['gas']
+massParticleGas = dump.massParticles['gas']
+massParticleDust = dump.massParticles['dust']
+massParticleDust[1] = massParticleDust[0]  # TODO: hack for broken splash to ascii
+massParticleSink = dump.massParticles['sink']
+
+positionGas = dump.position['gas']
+velocityGas = dump.velocity['gas']
+momentumGas = massParticleGas * velocityGas
+
+angularMomentumGas = np.cross(positionGas, momentumGas)
+smoothingLengthGas = dump.smoothingLength['gas']
+
+# TODO: get from .setup or HDF5 file
+grainDens = np.array([3., 3.]) / unitDens
+grainSize = np.array([0.01, 0.1]) / unitDist
 
 #--- Radial binning
 
@@ -38,32 +50,49 @@ rOut = 200
 
 dR = (rOut - rIn)/(nBins - 1)
 radius = np.linspace(rIn, rOut, nBins)
-cylindricalRadius = np.linalg.norm(dump.position['gas'][:,0:2], axis=1)
+
+cylindricalRadiusGas = np.linalg.norm(dump.position['gas'][:,0:2], axis=1)
+
+nDustTypes = len(dump.nParticles['dust'])
+cylindricalRadiusDust = list()
+for i in range(nDustTypes):
+    cylindricalRadiusDust.append(np.linalg.norm(dump.position['dust'][i][:,0:2], axis=1))
 
 #--- Surface density
 
-surfaceDensity  = np.empty_like(radius)
-averageDensity  = np.empty_like(radius)
-meanHeight      = np.empty_like(radius)
-scaleHeight     = np.empty_like(radius)
+surfaceDensityGas  = np.empty_like(radius)
+averageDensityGas  = np.empty_like(radius)
+meanHeightGas      = np.empty_like(radius)
+scaleHeightGas     = np.empty_like(radius)
+
+surfaceDensityDust = [np.empty_like(radius) for i in range(nDustTypes)]
 
 for idx, R in enumerate(radius):
 
-    indicies = np.where((cylindricalRadius < R + dR/2) & \
-                        (cylindricalRadius > R - dR/2))[0]
-
-    npart = len(indicies)
-
     area = np.pi * ( (R + dR/2)**2 - (R - dR/2)**2 )
-    surfaceDensity[idx] = massGasParticle * npart / area
 
-    meanHeight[idx]  = np.sum(position[indicies, 2])/npart
-    scaleHeight[idx] = np.sqrt(np.sum( ( position[indicies, 2] \
-                                       - meanHeight[idx] )**2 ) / (npart - 1))
+    indiciesGas = np.where((cylindricalRadiusGas < R + dR/2) & \
+                        (cylindricalRadiusGas > R - dR/2))[0]
 
-    averageDensity[idx] = np.sum( density_from_smoothing_length( \
-            smoothingLength[indicies], massGasParticle ) ) / npart
+    nPartGas = len(indiciesGas)
 
+    surfaceDensityGas[idx] = massParticleGas * nPartGas / area
+
+    meanHeightGas[idx]  = np.sum(positionGas[indiciesGas, 2])/nPartGas
+    scaleHeightGas[idx] = np.sqrt(np.sum( ( positionGas[indiciesGas, 2] \
+                                       - meanHeightGas[idx] )**2 ) / (nPartGas - 1))
+
+    averageDensityGas[idx] = np.sum( density_from_smoothing_length( \
+            smoothingLengthGas[indiciesGas], massParticleGas ) ) / nPartGas
+
+    for i in range(nDustTypes):
+
+        indiciesDust = np.where((cylindricalRadiusDust[i] < R + dR/2) & \
+                            (cylindricalRadiusDust[i] > R - dR/2))[0]
+
+        nPartDust = len(indiciesDust)
+
+        surfaceDensityDust[i][idx] = massParticleDust[i] * nPartDust / area
 
 
 
@@ -76,7 +105,7 @@ angularMomentum = np.cross(position, momentum)
 angularMomentumTotal = np.sum(angularMomentum, axis=0)
 angularMomentumTotalMagnitude = np.linalg.norm(angularMomentumTotal)
 
-angmom = angularMomentumTotalMagnitude * umass * udist**2 / utime
+angmom = angularMomentumTotalMagnitude * unitMass * unitDist**2 / unitTime
 print(f'Total angular momentum in gas disc is {angmom:g} g.cm^2/s')
 
 nSinks = dump.nParticles['sink']
@@ -87,6 +116,6 @@ for i in range(nSinks):
 
 angularMomentum = np.cross(position, momentum)
 angularMomentumTotalMagnitude = np.sum(angularMomentum, axis=1)
-angularMomentumTotalMagnitudeCGS = angularMomentumTotalMagnitude * umass * udist**2 / utime
+angularMomentumTotalMagnitudeCGS = angularMomentumTotalMagnitude * unitMass * unitDist**2 / unitTime
 for idx, angmom in enumerate(angularMomentumTotalMagnitudeCGS):
     print(f'Total angular momentum in sink {idx} is {angmom:g} g.cm^2/s')
