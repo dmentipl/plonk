56be4daf1cee22d3d4d4152ff10207636238c6fa
Daniel Mentiplay <d.mentiplay@gmail.com>
2019-01-22 15:47:36 +1100

    dump.py: now can read positions, velocities, smoothing length, mass, dustfrac

--- dump.py
+++ dump.py
@@ -9,19 +9,23 @@ import numpy as np
 from constants import constants
 from itypes import iTypes
 
+positionIndex = slice(0, 3)
+massIndex = 3
+smoothingLengthIndex = 4
+velocityIndex = slice(6, 9)
+dustFracIndexStart = 9
+
 class Dump:
     '''
     Dump class represents a dump file (ascii only).
     '''
 
-    # pylint: disable=too-many-instance-attributes
-
     def __init__(self, filename):
 
         self.filename = filename
-        self._read_file()
-        self._get_from_header()
-        self._get_from_data()
+        header, data = self._read_file()
+        self._get_from_header(header)
+        self._get_from_data(data)
 
     def _read_file(self):
 
@@ -39,24 +43,23 @@ class Dump:
                 else:
                     break
 
-        self.header = header
-
         #--- Data
 
-        self.data = np.loadtxt(self.filename)
+        data = np.loadtxt(self.filename)
 
-    def _get_from_header(self):
+        #--- Return
 
-        header = self.header
+        return header, data
+
+    def _get_from_header(self, header):
 
         #--- Column labels
 
         columnLabels = header[-1].split('  ')
         columnLabels = [col for col in columnLabels if col != '']
         columnLabels = [col[1:] if col[0] == ' ' else col for col in columnLabels]
-        self.columnLabels = columnLabels
 
-        self.nColumns = len(self.columnLabels)
+        nColumns = len(columnLabels)
 
         #--- Units
 
@@ -74,8 +77,8 @@ class Dump:
 
         units['time'] = timeUnit
 
-        distUnit = float(self.header[6].split()[0])
-        distUnitLabel = self.header[7].split()[0][1:-1]
+        distUnit = float(header[6].split()[0])
+        distUnitLabel = header[7].split()[0][1:-1]
 
         if distUnitLabel in ['au']:
             distUnit *= constants.au
@@ -86,8 +89,8 @@ class Dump:
 
         units['dist'] = distUnit
 
-        massUnit = float(self.header[6].split()[3])
-        massUnitLabel = self.header[7].split()[3][1:-1]
+        massUnit = float(header[6].split()[3])
+        massUnitLabel = header[7].split()[3][1:-1]
 
         if massUnitLabel in ['g']:
             pass
@@ -96,8 +99,8 @@ class Dump:
 
         units['mass'] = massUnit
 
-        velocityUnit = float(self.header[6].split()[6])
-        velocityUnitLabel = self.header[7].split()[6][1:-1]
+        velocityUnit = float(header[6].split()[6])
+        velocityUnitLabel = header[7].split()[6][1:-1]
 
         if velocityUnitLabel in ['cm/s']:
             pass
@@ -108,9 +111,7 @@ class Dump:
 
         self.units = units
 
-    def _get_from_data(self):
-
-        data = self.data
+    def _get_from_data(self, data):
 
         iGas = iTypes.iGas
         iSink = iTypes.iSink
@@ -119,17 +120,87 @@ class Dump:
 
         #--- Number of particles
 
-        self.nPart = data.shape[0]
+        nParticles = dict()
 
         itype = data[:,-1]
 
-        self.nGas = len(np.where(itype == iGas)[0])
-        self.nSink = len(np.where(itype == iSink)[0])
+        nParticles['gas'] = len(np.where(itype == iGas)[0])
+        nParticles['sink'] = len(np.where(itype == iSink)[0])
 
-        self.nDust = list()
+        nDust = list()
         for i in range(iDust, maxDustTypes+1):
             l = len(np.where(itype == i)[0])
             if l > 0:
-                self.nDust.append(l)
+                nDust.append(l)
+        nParticles['dust'] = nDust
+        nDustTypes = len(nDust)
 
-        self.nDustTypes = len(self.nDust)
+        self.nParticles = nParticles
+
+        #--- Mass of particles
+
+        massParticles = dict()
+
+        massParticles['gas'] = data[np.where(itype == iGas)[0][0], 3]
+
+        massSink = list()
+        for i in np.where(itype == iSink)[0]:
+            massSink.append(data[i, 3])
+        massParticles['sink'] = massSink
+
+        massDust = list()
+        for i in range(iDust, iDust + nDustTypes):
+            massDust.append(data[np.where(itype == i)[0][0], 3])
+        massParticles['dust'] = massDust
+
+        self.massParticles = massParticles
+
+        #--- Position
+
+        position = dict()
+
+        position['gas'] = data[np.where(itype == iGas), positionIndex][0]
+        position['sink'] = data[np.where(itype == iSink), positionIndex][0]
+
+        positionDust = list()
+        for i in range(iDust, iDust + nDustTypes):
+            positionDust.append(data[np.where(itype == i)[0], positionIndex])
+        position['dust'] = positionDust
+
+        self.position = position
+
+        #--- Velocity
+
+        velocity = dict()
+
+        velocity['gas'] = data[np.where(itype == iGas), velocityIndex][0]
+        velocity['sink'] = data[np.where(itype == iSink), velocityIndex][0]
+
+        velocityDust = list()
+        for i in range(iDust, iDust + nDustTypes):
+            velocityDust.append(data[np.where(itype == i)[0], velocityIndex])
+        velocity['dust'] = velocityDust
+
+        self.velocity = velocity
+
+        #--- Smoothing length
+
+        smoothingLength = dict()
+
+        smoothingLength['gas'] = data[np.where(itype == iGas),
+                                      smoothingLengthIndex][0]
+        smoothingLength['sink'] = data[np.where(itype == iSink),
+                                       smoothingLengthIndex][0]
+
+        smoothingLengthDust = list()
+        for i in range(iDust, iDust + nDustTypes):
+            smoothingLengthDust.append(data[np.where(itype == i)[0],
+                                            smoothingLengthIndex])
+        smoothingLength['dust'] = smoothingLengthDust
+
+        self.smoothingLength = smoothingLength
+
+        #--- Dust fraction
+
+        dustFracIndex = slice(dustFracIndexStart, dustFracIndexStart + nDustTypes)
+        self.dustFrac = data[np.where(itype == iGas), dustFracIndex][0]
