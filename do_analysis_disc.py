ed0d5f86d6842ee4215e8eafe56abb3569f6ab6f
Daniel Mentiplay <d.mentiplay@gmail.com>
2019-01-24 16:56:33 +1100

    do_analysis_disc.py: added some print statements to let the user know what's happening
    Former-commit-id: 65ed13ee3e14dcc921ae90197014e907689f1e63

--- do_analysis_disc.py
+++ do_analysis_disc.py
@@ -17,9 +17,13 @@ dumpFilePrefix = 'disc_00000'  # TODO: get dump filename as input
 
 #--- Read dump file
 
+print('\nReading in data from dumpfile: ' + dumpFilePrefix + '\n')
+
 dump = Dump(dumpFilePrefix)
 
 #--- Perform analysis
+
+print('Performing disc analysis on dumpfile: ' + dumpFilePrefix + '\n')
 
 analysis = disc_analysis(dump)
 
@@ -34,3 +38,22 @@ twist           = analysis[7]
 psi             = analysis[8]
 eccentricity    = analysis[9]
 Stokes          = analysis[10]
+
+output = \
+'''
+The following variables are available:
+
+  - radialBins
+  - surfaceDensity
+  - midplaneDensity
+  - smoothingLength
+  - scaleHeight
+  - angularMomentum
+  - tilt
+  - twist
+  - psi
+  - eccentricity
+  - Stokes
+'''
+
+print(output)
