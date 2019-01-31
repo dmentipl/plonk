d4eea597ec8cb2b31a9cc9090bc03d2607794c1a
Daniel Mentiplay <d.mentiplay@gmail.com>
2019-01-31 18:01:28 +1100

    more visualization work
    Former-commit-id: 8742e3f1c16fc5591593da53b63401ad68084fd4

--- plonk/visualization/image.py
+++ plonk/visualization/image.py
@@ -6,6 +6,7 @@ Daniel Mentiplay, 2019.
 
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 import numpy as np
 
 from ..particles import density_from_smoothing_length
@@ -16,10 +17,10 @@ set_interpolation_weights = splash.set_interpolation_weights
 
 # ---------------------------------------------------------------------------- #
 
-cmap            = 'inferno'
+cmap            = 'gist_heat'
 densityWeighted = False
-npixx           = 1000
-npixy           = 1000
+npixx           = 512
+npixy           = 512
 
 # ---------------------------------------------------------------------------- #
 
@@ -75,12 +76,51 @@ class Image:
 
         return interpolationWeights
 
-    def _interpolate_to_pixelgrid(self):
+    def plot(self, horizontalAxis, verticalAxis, render, ax=None, scale='lin',
+             vmin=None, vmax=None, fpeak=None, limit=-1, limits=None,
+             title=None, colorbar=False):
+        '''
+        Make image.
+        '''
 
-        # TODO: choose fluid type
-        # TODO: choose xData, yData, render
-        xData = self.gas.position[:, 0]
-        yData = self.gas.position[:, 1]
+        # TODO: choose fluid type: gas, dust1, dust2, ...
+        # TODO: choose xData, yData, renderData
+
+        if horizontalAxis == 'x':
+            ix = 0
+        elif horizontalAxis == 'y':
+            ix = 1
+        elif horizontalAxis == 'z':
+            ix = 2
+        else:
+            raise ValueError('horizontalAxis should be "x", "y", or "z"')
+
+        if verticalAxis == 'x':
+            iy = 0
+        elif verticalAxis == 'y':
+            iy = 1
+        elif verticalAxis == 'z':
+            iy = 2
+        else:
+            raise ValueError('verticalAxis should be "x", "y", or "z"')
+
+        xData = self.gas.position[:, ix]
+        yData = self.gas.position[:, iy]
+
+        if render in ['rho', 'dens', 'density']:
+            renderData = density_from_smoothing_length(self.gas.smoothingLength,
+                                                       self.gas.mass,
+                                                       self.hfact)
+        else:
+            raise ValueError('render unknown')
+
+        # TODO: set these according to fluid choice
+        interpolationWeights = self.interpolationWeights['gas']
+        smoothingLength = self.gas.smoothingLength
+
+        # TODO: set x and y range (check units)
+        xRange = [-500, 500]
+        yRange = [-500, 500]
 
         # TODO: choose these options
         normalise = False
@@ -88,60 +128,20 @@ class Image:
         periodicx = False
         periodicy = False
 
-        smoothingLength = self.gas.smoothingLength
-        render = density_from_smoothing_length(smoothingLength, self.gas.mass,
-                                               self.hfact)
-
-        itype = np.ones_like(xData)
-        interpolationWeights = self.interpolationWeights['gas']
-        npart = len(smoothingLength)
-
-        maxx      = np.max(xData)
-        maxy      = np.max(yData)
-        xmin      = - maxx
-        ymin      = - maxy
-        pixwidthx = 2*maxx / npixx
-        pixwidthy = 2*maxy / npixy
-
-        imageData = np.zeros((npixx, npixy), dtype=np.float32, order='F')
-
-        interpolate2d(x=xData,
-                      y=yData,
-                      hh=smoothingLength,
-                      weight=interpolationWeights,
-                      dat=render,
-                      itype=itype,
-                      npart=npart,
-                      xmin=xmin,
-                      ymin=ymin,
-                      datsmooth=imageData,
-                      npixx=npixx,
-                      npixy=npixy,
-                      pixwidthx=pixwidthx,
-                      pixwidthy=pixwidthy,
-                      normalise=normalise,
-                      exact=exact,
-                      periodicx=periodicx,
-                      periodicy=periodicy)
-
-        return imageData
-
-    def plot(self):
-        '''
-        Make image.
-        '''
+        imageData = _interpolate_to_pixelgrid(
+            xData, yData, renderData, interpolationWeights, smoothingLength,
+            xRange, yRange, normalise, exact, periodicx, periodicy)
 
         # TODO: add options
         # TODO: choose what to plot
         # TODO: add docs
         # TODO: check if need to interpolate again
 
-        imageData = self._interpolate_to_pixelgrid()
+        extent = xRange + yRange
 
-        vmin = None
-        vmax = None
-        fpeak = None
-        scale = 'log'
+        if ax is None:
+            ax = plt.gca()
+
         _scale = 'lin'
 
         if vmax is None:
@@ -163,4 +163,83 @@ class Image:
         else:
             raise ValueError("Unknown color scale: " + scale)
 
-        plt.imshow(imageData, norm=norm, origin='lower', cmap=cmap)
+        img = ax.imshow(imageData, norm=norm, extent=extent, origin='lower',
+                        cmap=cmap)
+
+        # TODO: set labels correctly
+        xlabel = r'x [au]'
+        ylabel = r'y [au]'
+
+        ax.set_xlabel(xlabel)
+        ax.set_ylabel(ylabel)
+
+        if limit > 0:
+            limits = [-limit,limit,-limit,limit]
+
+        if limits is not None:
+            ax.set_xlim(limits[0],limits[1])
+            ax.set_ylim(limits[2],limits[3])
+
+        if title is not None:
+            ax.set_title(title)
+
+        cb = None
+        if colorbar:
+            divider = make_axes_locatable(ax)
+            cax = divider.append_axes("right", size="5%", pad=0.05)
+            cb = plt.colorbar(img, cax=cax)
+
+        return img, cb
+
+    def _convert_units(self):
+        '''
+        Convert units.
+        '''
+
+        # TODO: write this
+
+    def _calculate_quantity(self):
+        '''
+        Calculate an extra quantity.
+        '''
+
+        # TODO: write this
+
+def _interpolate_to_pixelgrid(xData, yData, renderData,
+                              interpolationWeights, smoothingLength, xRange,
+                              yRange, normalise=False, exact=False,
+                              periodicx=False, periodicy=False):
+
+    itype = np.ones_like(xData)
+    npart = len(smoothingLength)
+
+    xmax      = xRange[1]
+    ymax      = yRange[1]
+    xmin      = xRange[0]
+    ymin      = yRange[0]
+    pixwidthx = (xmax - xmin) / npixx
+    pixwidthy = (ymax - ymin) / npixy
+
+    imageData = np.zeros((npixx, npixy), dtype=np.float32, order='F')
+
+    interpolate2d(x=xData,
+                  y=yData,
+                  hh=smoothingLength,
+                  weight=interpolationWeights,
+                  dat=renderData,
+                  itype=itype,
+                  npart=npart,
+                  xmin=xmin,
+                  ymin=ymin,
+                  datsmooth=imageData,
+                  npixx=npixx,
+                  npixy=npixy,
+                  pixwidthx=pixwidthx,
+                  pixwidthy=pixwidthy,
+                  normalise=normalise,
+                  exact=exact,
+                  periodicx=periodicx,
+                  periodicy=periodicy)
+
+    return imageData
+
