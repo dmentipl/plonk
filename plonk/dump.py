b13fdd7e847f825787734c4ffa75f5d55655a216
Daniel Mentiplay <d.mentiplay@gmail.com>
2019-02-18 12:18:27 +1100

    (dump) fix issues with HDF5 dump reader; now set mass correctly, set is_full_dump correctly
    Former-commit-id: a59173812cfdfbffcf375269a75c94250c7b1c51

--- plonk/dump.py
+++ plonk/dump.py
@@ -10,9 +10,8 @@ import h5py
 import numpy as np
 import pandas as pd
 
-from .particles import density_from_smoothing_length, I_DUST
+from .particles import density_from_smoothing_length, I_GAS, I_DUST
 from .units import Units
-from .utils import print_warning
 
 # ---------------------------------------------------------------------------- #
 
@@ -70,7 +69,6 @@ class Dump:
 
         if file_extension == 'h5':
             dump_file_format = 'HDF5'
-            print_warning('HDF5 dump reader not fully implemented')
 
         elif file_extension == 'ascii':
             dump_file_format = 'ASCII'
@@ -129,7 +127,7 @@ class Dump:
 
         if dump_file_format == 'HDF5':
 
-            self._read_arrays_from_hdf5(dump_file_name, is_full_dump)
+            self._read_arrays_from_hdf5(dump_file_name)
 
         elif dump_file_format == 'ASCII':
 
@@ -177,14 +175,17 @@ class Dump:
 
             self.sinks = sinks
 
-    def _read_arrays_from_hdf5(self, dump_file_name, is_full_dump):
+    def _read_arrays_from_hdf5(self, dump_file_name):
 
+        n_dust_large = self.parameters['ndustlarge']
         n_dust_types = self.parameters['ndustsmall'] + self.parameters['ndustlarge']
 
         n_sinks = self.parameters['nptmass']
         contains_sinks = bool(n_sinks > 0)
 
         f = h5py.File(dump_file_name, 'r')
+
+        is_full_dump = bool( 'vxyz' in f['particles'] )
 
         #--- Particles
 
@@ -195,9 +196,7 @@ class Dump:
         self.particles['y'] = particles['xyz'][:, 1]
         self.particles['z'] = particles['xyz'][:, 2]
 
-        # TODO: set particle masses
-        self.particles['m'] = 1.
-        print_warning('Mass not set properly')
+        self.particles['m'] = np.nan
 
         self.particles['h'] = particles['h']
 
@@ -206,7 +205,10 @@ class Dump:
             self.particles['m'],
             hfact=self.parameters['hfact'])
 
-        self.particles['P'] = particles['pressure']
+        if is_full_dump:
+            self.particles['P'] = particles['pressure']
+        else:
+            self.particles['P'] = np.nan
 
         if is_full_dump:
             self.particles['vx'] = particles['vxyz'][:, 0]
@@ -223,12 +225,28 @@ class Dump:
                 tag1 = 'dustfrac' + str(n+1)
                 tag2 = 'tstop' + str(n+1)
                 self.particles[tag1] = particles['dustfrac'][:, n]
-                self.particles[tag2] = particles['tstop'][:, n]
+                if is_full_dump:
+                    self.particles[tag2] = particles['tstop'][:, n]
+                else:
+                    self.particles[tag2] = np.nan
 
         if is_full_dump:
             self.particles['divv']  = particles['divv']
             self.particles['dt']    = particles['dt']
-            self.particles['itype'] = particles['itype']
+
+        else:
+            self.particles['divv']  = np.nan
+            self.particles['dt']    = np.nan
+
+        self.particles['itype'] = particles['itype']
+
+        self.particles.loc[self.particles['itype']==I_GAS, 'm'] = \
+            self.parameters['massoftype'][I_GAS-1]
+
+        if n_dust_large  > 0:
+            for n in range(n_dust_large):
+                self.particles.loc[self.particles['itype']==I_DUST+n, 'm'] = \
+                    self.parameters['massoftype'][I_DUST+n-1]
 
         #--- Sinks
 
@@ -275,10 +293,7 @@ def _read_header_from_hdf5(dump_file_name):
 
     f.close()
 
-    is_full_dump = True
-    print_warning('HDF5 dump reader work in progress: assuming full dump')
-
-    return header, is_full_dump
+    return header
 
 def _read_header_from_showheader(header_file_name):
 
