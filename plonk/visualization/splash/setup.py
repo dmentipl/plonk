194bff680e0b209c09a6784dd5976c7b8d34e0e5
Daniel Mentiplay <d.mentiplay@gmail.com>
2019-03-08 15:21:11 +1100

    Moved most of Makefile to setup.py
    Former-commit-id: 3b8513d6b759447e2a9b926ea8b0eda5c17e62c4

--- plonk/visualization/splash/setup.py
+++ plonk/visualization/splash/setup.py
@@ -3,18 +3,83 @@ setup.py
 '''
 
 import os.path
+import subprocess
 
 from setuptools import setup, Extension
 from Cython.Distutils import build_ext
 import numpy as np
 
-here = os.path.dirname(os.path.abspath(__file__))
+OPENMP          = True
+SYSTEM          = 'gfortran'
+DOUBLEPRECISION = False
+DEBUG           = False
+KERNEL          = None
+
+if SYSTEM == 'gfortran':
+    FC        = 'gfortran'
+    FFLAGS    = '-O3'
+    DBLFLAGS  = '-fdefault-real-8 -fdefault-double-8'
+    DEBUGFLAG = '-Wall -Wextra -pedantic -g -frange-check -fcheck=all \
+                -fbacktrace -finit-real=NaN #-ffpe-trap=invalid,zero,overflow'
+    OMPFLAGS  = '-fopenmp'
+else:
+    raise ValueError('Compiler not available; try gfortran')
+
+if KERNEL:
+    if KERNEL == 'cubic':
+        FFLAGS += ' -DKERNEL=1'
+    elif KERNEL == 'quartic':
+        FFLAGS += ' -DKERNEL=2'
+    elif KERNEL == 'quintic':
+        FFLAGS += ' -DKERNEL=3'
+    elif KERNEL == 'quartic2h':
+        FFLAGS += ' -DKERNEL=4'
+    elif KERNEL == 'wendlandc2':
+        FFLAGS += ' -DKERNEL=5'
+    elif KERNEL == 'wendlandc4':
+        FFLAGS += ' -DKERNEL=6'
+    elif KERNEL == 'wendlandc6':
+        FFLAGS += ' -DKERNEL=7'
+    else:
+        FFLAGS += ' -DKERNEL=99'
+else:
+    FFLAGS += ' -DKERNEL=99'
+
+if DOUBLEPRECISION:
+    FFLAGS += ' ' + DBLFLAGS
+    print("DOUBLEPRECISION=yes will probably fail")
+
+if DEBUG:
+    FFLAGS += ' ' + DEBUGFLAG
+
+if OPENMP:
+    FFLAGS += ' ' + OMPFLAGS
+
+print()
+print(" Compiling SPLASH interpolation routines")
+print()
+print(f"   Fortran compiler: {FC}")
+print(f"   Fortran flags:    {FFLAGS}")
+print()
+print(" See Makefile for compile time options")
+print()
+
+local_dir = os.path.dirname(os.path.abspath(__file__))
+
+ffiles = ['splash.F90', 'splash_wrapper.f90']
+fc_command = FC + ' ' + FFLAGS + ' -c '
+fl_command = FC + ' -shared -fPIC ' + FFLAGS + ' -o libsplashwrapper.so *.o'
+
+for ffile in ffiles:
+    subprocess.run(fc_command + ffile, shell=True)
+
+subprocess.run(fl_command, shell=True)
 
 ext_modules = [Extension("_splash",
                          ["splash_wrapper.pyx"],
                          libraries=['splashwrapper', 'gfortran'],
-                         library_dirs=[here],
-                         runtime_library_dirs=[here],
+                         library_dirs=[local_dir],
+                         runtime_library_dirs=[local_dir],
                         )]
 
 setup(
