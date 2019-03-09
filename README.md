96fcec447f2dc597ac0728eabfb1e08607174b3a
Daniel Mentiplay <d.mentiplay@gmail.com>
2019-03-09 14:05:48 +1100

    Now setup.py for Splash routines works for macOS systems without having to set DYLD_LIBRARY_PATH
    Former-commit-id: f099e6aa3a1a7e9eb18c437c26dd098160718715

--- README.md
+++ README.md
@@ -8,9 +8,4 @@ Install
 
 To use visualization tools you first must compile Splash Fortran interpolation routines. To do this, type `make` in the main repository directory.
 
-You must also set the `LD_LIBRARY_PATH` (for Linux) or `DYLD_LIBRARY_PATH` (for
-macOS). For example:
-
-```
-export DYLD_LIBRARY_PATH=DYLD_LIBRARY_PATH:~/plonk/plonk/visualization/splash
-```
+For Fortran-Python compatibility we use Cython and Fortran 2003+-C interoperability features
--- plonk/visualization/splash/setup.py
+++ plonk/visualization/splash/setup.py
@@ -5,6 +5,7 @@ Daniel Mentiplay, 2019.
 '''
 
 import os.path
+import platform
 import subprocess
 
 from setuptools import setup, Extension
@@ -16,19 +17,27 @@ DOUBLE = False
 DEBUG  = False
 KERNEL = 'cubic'
 
-FSOURCES = ['splash.F90', 'splash_wrapper.f90']
+FSOURCES  = ['splash.F90', 'splash_wrapper.f90'] # Fortran sources in order
+LIBSPLASH = 'libsplashwrapper.so'                # Splash library name
+
+local_dir = os.path.dirname(os.path.abspath(__file__))
 
 FC      = 'gfortran'
 FCFLAGS = ['-O3']
-FCLIBS  = ['-shared', '-fPIC', '-o libsplashwrapper.so', '*.o']
+FCLIBS  = ['-shared', '-fPIC', '-o '+LIBSPLASH, '*.o']
 
 KERNELS = ['cubic', 'quartic', 'quintic', 'quartic2h', 'wendlandc2',
            'wendlandc4', 'wendlandc6']
 
 if KERNEL:
+    kernel_set = False
     for idx, K in enumerate(KERNELS):
         if K == KERNEL:
+            kernel_set = True
             FCFLAGS += [f'-DKERNEL={idx+1}']
+            break
+    if not kernel_set:
+        FCFLAGS += ['-DKERNEL=99']
 else:
     FCFLAGS += ['-DKERNEL=99']
 
@@ -49,21 +58,30 @@ print()
 print(f'   Fortran compiler: {FC}')
 print(f'   Fortran flags:    {" ".join(FCFLAGS)}')
 print()
-print(' See Makefile for compile time options')
+print(' See setup.py for compile time options')
 print()
 
+# Compile
 for file in FSOURCES:
     subprocess.run(' '.join([FC] + FCFLAGS + ['-c', file]), shell=True)
 
+# Link to make library
 subprocess.run(' '.join([FC] + FCFLAGS + FCLIBS), shell=True)
 
-local_dir = os.path.dirname(os.path.abspath(__file__))
+# Fix for macOS systems
+extra_link_args = list()
+if platform.system() == 'Darwin':
+    subprocess.run('install_name_tool -id @rpath/'+LIBSPLASH+' ' + \
+                   local_dir+'/'+LIBSPLASH, shell=True)
+    extra_link_args.append('-Wl,-rpath,'+local_dir)
 
+# Make Splash library available to Python as _splash
 ext_modules = [Extension('_splash',
                          ['splash_wrapper.pyx'],
                          libraries=['splashwrapper', 'gfortran'],
                          library_dirs=[local_dir],
                          runtime_library_dirs=[local_dir],
+                         extra_link_args=extra_link_args,
                         )]
 
 setup(
