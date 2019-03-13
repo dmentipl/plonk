e17beab206a63b35e8886f8bcb8becdb0e608c30
Daniel Mentiplay <d.mentiplay@gmail.com>
2019-03-13 14:14:13 +1100

    (utils) rotate space around arbitrary axis
    Former-commit-id: 2a2a41f185d7a7c2ca46d0083f59629f463eeeec

--- plonk/utils.py
+++ plonk/utils.py
@@ -4,6 +4,8 @@ utils.py
 Daniel Mentiplay, 2019.
 '''
 
+import numpy as np
+
 def print_warning(message):
     '''
     Print formatted warning message.
@@ -27,3 +29,25 @@ def print_error(message):
          + l*'-' + '\n' \
          + message + '\n' \
          + l*'-' )
+
+def rotate_vector_arbitrary_axis(u, v, theta):
+    '''
+    Rotate a 3d vector (u) around an axis defined by another 3d vector (v)
+    by an angle (theta) using the Rodrigues rotation formula.
+
+    u can also be a numpy array of ndim 2, each row of which is a 3d vector.
+    '''
+
+    norm = np.linalg.norm(v)
+    if np.isclose(norm, 0.):
+        k = v
+    else:
+        k = v / norm
+
+    w = np.cross(k, u)
+
+    k = np.stack(u.shape[0]*[k])
+    dot_kv = np.sum(k*u, axis=1)
+    dot_kv = np.stack(3*[dot_kv]).T
+
+    return u*np.cos(theta) + w*np.sin(theta) + k*dot_kv*(1-np.cos(theta))
