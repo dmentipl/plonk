8984cfebcaa82d7339699d2f6ea4c58f910b0ade
Daniel Mentiplay <d.mentiplay@gmail.com>
2019-05-03 17:32:34 +1000

    Started Simulation class
    Former-commit-id: 7a7833fe9edffc5323f1ab6f952b77fdf1afc82f

---
+++ plonk/simulation.py
@@ -0,0 +1,23 @@
+"""
+simulation.py
+
+Daniel Mentiplay, 2019.
+"""
+
+
+class Simulation():
+    """
+    Smoothed particle hydrodynamics simulation data. Aggregates dump
+    files and other extra data.
+
+    Parameters
+    ----------
+    name : str
+        Directory containing dump files and auxiliary files.
+
+    Examples
+    --------
+    """
+
+    # TODO: implement
+    # TODO: add documentation
