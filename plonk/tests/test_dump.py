7aa3076752457a37e7e53c0ef7b53f09e473099d
Daniel Mentiplay <d.mentiplay@gmail.com>
2019-05-07 15:47:44 +1000

    Tidied up unittests
    Former-commit-id: 83270c51e2c51c309227612dac0761bde7e846ad

--- plonk/tests/test_dump.py
+++ plonk/tests/test_dump.py
@@ -7,29 +7,184 @@ import unittest
 
 import numpy as np
 
-from plonk.dump import Dump
-from .test_data.disc_00000 import DumpTest
+import plonk
 
 
-class TestDump(unittest.TestCase):
+class TestPhantomDump(unittest.TestCase):
     """Test reading Phantom HDF dump files."""
 
-    def test_read_dump(self):
-        """Testing reading Phantom HDF dump file."""
+    def test_read_dump_parameters(self):
+        """Testing reading Phantom HDF dump file parameters."""
 
-        file_dir = os.path.dirname(os.path.abspath(__file__))
-        test_dump = os.path.join(file_dir, 'test_data', 'disc_00000.h5')
-        dump = Dump(test_dump)
-        test_data = DumpTest()
+        test_file = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            'test_data',
+            'disc_00000.h5',
+        )
 
-        for para in dump.parameters:
-            if isinstance(dump.parameters[para], np.ndarray):
+        test_parameters = plonk.Dump(test_file).parameters
+
+        reference_parameters = {
+            'Bextx': 0.0,
+            'Bexty': 0.0,
+            'Bextz': 0.0,
+            'C_cour': 0.3,
+            'C_force': 0.25,
+            'RK2': 0.0037499999999999994,
+            'alpha': 0.23580036600499324,
+            'alphaB': 1.0,
+            'alphau': 1.0,
+            'angtot_in': 0.4156965032125065,
+            'dtmax': 1154.294847149428,
+            'dum': 0.0,
+            'etot_in': 0.0031034525718330545,
+            'fileident': b'fulldump: Phantom 1.2.1 7b32a1c (hydro+dust): '
+            b'26/02/2019 09:42:31.2',
+            'gamma': 1.0,
+            'get_conserv': -1.0,
+            'graindens': np.array(
+                [
+                    5049628.37866372,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                ]
+            ),
+            'grainsize': np.array(
+                [
+                    6.68449198e-14,
+                    0.00000000e00,
+                    0.00000000e00,
+                    0.00000000e00,
+                    0.00000000e00,
+                    0.00000000e00,
+                    0.00000000e00,
+                    0.00000000e00,
+                    0.00000000e00,
+                    0.00000000e00,
+                    0.00000000e00,
+                    0.00000000e00,
+                    0.00000000e00,
+                    0.00000000e00,
+                    0.00000000e00,
+                    0.00000000e00,
+                    0.00000000e00,
+                    0.00000000e00,
+                    0.00000000e00,
+                    0.00000000e00,
+                    0.00000000e00,
+                    0.00000000e00,
+                ]
+            ),
+            'hfact': 1.0,
+            'idust': 7,
+            'ieos': 3,
+            'iexternalforce': 0,
+            'isink': 0,
+            'majorv': 1,
+            'massoftype': np.array(
+                [
+                    1.0e-07,
+                    0.0e00,
+                    0.0e00,
+                    0.0e00,
+                    0.0e00,
+                    0.0e00,
+                    5.0e-09,
+                    0.0e00,
+                    0.0e00,
+                    0.0e00,
+                    0.0e00,
+                    0.0e00,
+                    0.0e00,
+                    0.0e00,
+                    0.0e00,
+                    0.0e00,
+                    0.0e00,
+                ]
+            ),
+            'mdust_in': np.array(
+                [
+                    0.0005,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                ]
+            ),
+            'microv': 1,
+            'minorv': 2,
+            'nblocks': 1,
+            'ndustlarge': 1,
+            'ndustsmall': 0,
+            'npartoftype': np.array(
+                [500000, 0, 0, 0, 0, 0, 100000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                dtype=np.int32,
+            ),
+            'nparttot': 600000,
+            'nptmass': 1,
+            'ntypes': 17,
+            'polyk2': 0.0,
+            'qfacdisc': 0.25,
+            'rhozero': 0.0,
+            'time': 0.0,
+            'tolh': 0.0001,
+            'totmom_in': 6.812299010933773e-20,
+            'udist': 14960000000000.0,
+            'umagfd': 8138.187949213826,
+            'umass': 1.9891e33,
+            'utime': 5022728.790082334,
+            'xmax': 0.5,
+            'xmin': -0.5,
+            'ymax': 0.5,
+            'ymin': -0.5,
+            'zmax': 0.5,
+            'zmin': -0.5,
+        }
+
+        for para in test_parameters:
+            if isinstance(test_parameters[para], np.ndarray):
                 np.testing.assert_allclose(
-                    dump.parameters[para], test_data.parameters[para]
+                    test_parameters[para], reference_parameters[para]
                 )
             else:
                 self.assertEqual(
-                    dump.parameters[para], test_data.parameters[para]
+                    test_parameters[para], reference_parameters[para]
                 )
 
 
