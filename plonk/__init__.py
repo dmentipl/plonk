f5b167ce0b34356bc9ce96b4c7732d7810c4a7b0
Daniel Mentiplay <d.mentiplay@gmail.com>
2019-05-30 09:32:09 +1000

    [skip ci] __init__ docstring
    Added info on Classes and Subpackages.
    
    
    Former-commit-id: 288b23ff4641484a4878743375a3ff020a1622c9

--- plonk/__init__.py
+++ plonk/__init__.py
@@ -8,11 +8,39 @@ particle hydrodynamics simulation data.
 Features
 --------
 
-  - Read in Phantom HDF dump files.
-  - Access particle and sink arrays as Numpy structured arrays.
-  - Access simulation parameters and units.
-  - Compute extra quantities on particles.
-  - Visualize data using interpolation provided by Splash.
+- Read in Phantom HDF dump files.
+- Read in global evolution files.
+- Encapsulate entire simulation data as Simulation object.
+- Access particle and sink arrays.
+- Access simulation parameters and units.
+- Compute extra quantities on particles.
+- Visualize data using interpolation provided by Splash.
+
+Classes
+-------
+
+- Dump
+    Represents a smoothed particle hydrodynamics dump file,
+    containing particles, sinks, and file header information.
+
+- Evolution
+    Represents globally averaged quantities as time series data.
+
+- Simulation
+    Represents an entire smoothed particle hydrodynamics simulation.
+    It contains instances of Dump and Evolution objects.
+
+- Visualization
+    Represents a visualization of a Dump object.
+
+Subpackages
+-----------
+
+- analysis
+    Contains functions for performing analysis on dump files.
+
+Documentation
+-------------
 
 See https://plonk.readthedocs.io/ for documentation. The source code is
 available at https://github.com/dmentipl/plonk.
