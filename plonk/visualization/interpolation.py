019104635de393bb66b6cc3a5bc10d3cf44aa2a4
Daniel Mentiplay <d.mentiplay@gmail.com>
2019-10-24 11:43:33 +1100

    WIP: Clean up interpolation function interfaces
    Now we pass in the x, y, z coordinates as separate arrays. This is to
    make it easier to use different coordinate systems. For example, x and y
    could be R and Z in cylindrical coordinates.
    
    We also pass in the x and y components of the vector quantity as
    separate arrays. Again this is to make it easier to use different
    coordinte systems.
    
    Note this breaks the Visualization class, so this commit is a WIP.

--- plonk/visualization/interpolation.py
+++ plonk/visualization/interpolation.py
@@ -3,12 +3,14 @@ Splash interpolation wrapper routines.
 
 There are two main routines. One for interpolation of scalar fields and
 one for interpolation of vector fields. They both make calls to the
-Splash Fortran libraries via cython.
+Splash Fortran libraries via Cython.
 """
 
 import warnings
+from typing import Optional, Tuple
 
 import numpy as np
+from numpy import ndarray
 
 try:
     from splash import splash
@@ -19,139 +21,112 @@ IVERBOSE = -2
 
 
 def scalar_interpolation(
-    positions,
-    smoothing_length,
-    weights,
-    scalar_data,
-    particle_mass,
-    horizontal_range,
-    vertical_range,
+    x_coordinate: ndarray,
+    y_coordinate: ndarray,
+    z_coordinate: ndarray,
+    smoothing_length: ndarray,
+    smoothing_length_factor: float,
+    scalar_data: ndarray,
+    particle_mass: ndarray,
+    x_range: Tuple[float, float],
+    y_range: Tuple[float, float],
     *,
-    number_pixels=None,
-    cross_section=None,
-    slice_position=None,
-    perspective=None,
-    observer_distance=None,
-    opacity=None,
-    normalize=None,
-    accelerate=None,
-    integrated_z=None,
+    number_pixels: Tuple[float, float] = (512, 512),
+    density_weighted: bool = False,
+    cross_section: bool = False,
+    slice_position: float = 0.0,
+    perspective: bool = False,
+    observer_distance: float = 0.0,
+    opacity: bool = False,
+    normalize: bool = False,
+    accelerate: bool = False,
+    integrated_z: Optional[float] = None,
     **kwargs,
-):
+) -> ndarray:
     """
     Interpolate a scalar quantity to a pixel grid.
 
     Parameters
     ----------
-    positions : numpy.ndarray
-        Particle positions where columns are Cartesian 'x', 'y', 'z'.
-    smoothing_length : numpy.ndarray
+    x_coordinate
+        Particle coordinate for x-axis in interpolation.
+    y_coordinate
+        Particle coordinate for y-axis in interpolation.
+    z_coordinate
+        Particle coordinate for depth in interpolation.
+    smoothing_length
         Particle smoothing length.
-    weights : numpy.ndarray
-        Interpolation weights.
-    scalar_data : numpy.ndarray
+    smoothing_length_factor
+        The smoothing length factor.
+    scalar_data
         A scalar quantity on the particles to interpolate.
-    particle_mass : numpy.ndarray
+    particle_mass
         The particle mass on each particle.
-    horizontal_range : list of float
-        The horizontal range as [xmin, xmax].
-    vertical_range : list of float
-        The vertical range as [ymin, ymax].
+    x_range
+        The range in the x-direction as (xmin, xmax).
+    y_range
+        The vertical range as (ymin, ymax).
 
     Optional parameters
     -------------------
-    number_pixels : list of float (default [512, 512])
+    number_pixels
         The pixel grid to interpolate the scalar quantity to, as
-        [npixx, npixy].
-    cross_section : bool (default False)
+        (npixx, npixy).
+    density_weighted
+        Use density weighted interpolation.
+    cross_section
         Turn on cross section rendering.
-    slice_position : float (default 0.0)
+    slice_position
         Slice position as a z-value.
-    perspective : bool (default False)
+    perspective
         Turn on perspective rendering.
-    observer_distance : float (default 0.0)
+    observer_distance
         Distance from the screen to the observer. This turns on 3D
         perspective rendering.
-    opacity : bool (default False)
+    opacity
         Turn on opacity rendering.
-    normalize : bool (default False)
+    normalize
         Use normalized interpolation.
-    accelerate : bool (default False)
+    accelerate
         Use accelerated interpolation.
-    integrated_z : float (default None)
+    integrated_z
         Z unit for projection plots.
 
     Returns
     -------
-    numpy.ndarray
+    ndarray
         An array of scalar quantities interpolated to a pixel grid with
         shape (npixx, npixy).
     """
-
-    if number_pixels is None:
-        npixx = 512
-        npixy = 512
-    else:
-        npixx = number_pixels[0]
-        npixy = number_pixels[1]
-
-    projection = False
-    if cross_section is None:
-        cross_section = False
-    if perspective is None:
-        perspective = False
-    if not cross_section and not perspective:
-        projection = True
-
-    if cross_section and slice_position is None:
-        slice_position = 0.0
+    npixx = number_pixels[0]
+    npixy = number_pixels[1]
+    projection = not cross_section and not perspective
     zslice = slice_position
-
-    if perspective and observer_distance is None:
-        observer_distance = 0.0
-
-    if observer_distance is not None:
-        dscreen = observer_distance
-        zobserver = observer_distance
-    else:
-        dscreen = 0.0
-        zobserver = 0.0
-
-    if opacity is None:
-        opacity = False
-
-    if opacity:
-        if observer_distance is None:
-            raise ValueError('observer_distance cannot be None for opacity rendering')
-
-    if normalize is None:
-        normalise = False
-    else:
-        normalise = normalize
-
-    if accelerate is None:
-        useaccelerate = False
-    else:
-        useaccelerate = accelerate
-
+    dscreen = observer_distance
+    zobserver = observer_distance
+    normalise = normalize
+    useaccelerate = accelerate
     npart = len(smoothing_length)
-    itype = np.ones(npart, dtype=np.int32)
-
-    xmin = horizontal_range[0]
-    ymin = vertical_range[0]
-    xmax = horizontal_range[1]
-    ymax = vertical_range[1]
+    xmin = x_range[0]
+    ymin = y_range[0]
+    xmax = x_range[1]
+    ymax = y_range[1]
     pixwidthx = (xmax - xmin) / npixx
     pixwidthy = (ymax - ymin) / npixy
+    if density_weighted:
+        weights = particle_mass / smoothing_length ** 2
+    else:
+        weights = np.ones(npart) / smoothing_length_factor
 
     # Splash routines expect single precision
-    x = np.array(positions[:, 0], dtype=np.single)
-    y = np.array(positions[:, 1], dtype=np.single)
-    z = np.array(positions[:, 2], dtype=np.single)
-    pmass = np.array(particle_mass, dtype=np.single)
+    x = np.array(x_coordinate, dtype=np.single)
+    y = np.array(y_coordinate, dtype=np.single)
+    z = np.array(z_coordinate, dtype=np.single)
     hh = np.array(smoothing_length, dtype=np.single)
-    dat = np.array(scalar_data, dtype=np.single)
+    pmass = np.array(particle_mass, dtype=np.single)
     weight = np.array(weights, dtype=np.single)
+    dat = np.array(scalar_data, dtype=np.single)
+    itype = np.ones(npart, dtype=np.int32)
 
     if projection or perspective:
         if not opacity:
@@ -238,118 +213,112 @@ def scalar_interpolation(
 
 
 def vector_interpolation(
-    positions,
-    smoothing_length,
-    weights,
-    vector_data,
-    horizontal_range,
-    vertical_range,
+    x_coordinate: ndarray,
+    y_coordinate: ndarray,
+    z_coordinate: ndarray,
+    smoothing_length: ndarray,
+    smoothing_length_factor: float,
+    x_vector_data: ndarray,
+    y_vector_data: ndarray,
+    particle_mass: ndarray,
+    x_range: Tuple[float, float],
+    y_range: Tuple[float, float],
     *,
-    number_pixels=None,
-    cross_section=None,
-    slice_position=None,
-    perspective=None,
-    observer_distance=None,
-    normalize=None,
-    integrated_z=None,
+    number_pixels: Tuple[float, float] = (512, 512),
+    density_weighted: bool = False,
+    cross_section: bool = False,
+    slice_position: float = 0.0,
+    perspective: bool = False,
+    observer_distance: float = 0.0,
+    normalize: bool = False,
+    integrated_z: Optional[float] = None,
     **kwargs,
-):
+) -> ndarray:
     """
     Interpolate a vector quantity to a pixel grid.
 
     Parameters
     ----------
-    positions : numpy.ndarray
-        Particle positions where columns are Cartesian 'x', 'y', 'z'.
-    smoothing_length : numpy.ndarray
+    x_coordinate
+        Particle coordinate for x-axis in interpolation.
+    y_coordinate
+        Particle coordinate for y-axis in interpolation.
+    z_coordinate
+        Particle coordinate for depth in interpolation.
+    smoothing_length
         Particle smoothing length.
-    weights : numpy.ndarray
-        Interpolation weights.
-    vector_data : numpy.ndarray
-        A vector quantity on the particles to interpolate.
-    horizontal_range : list of float
-        The horizontal range as [xmin, xmax].
-    vertical_range : list of float
-        The vertical range as [ymin, ymax].
+    smoothing_length_factor
+        The smoothing length factor.
+    x_vector_data
+        The x-component of a vector quantity on the particles to
+        interpolate. This is the x-component with reference to the
+        coordinate system.
+    y_vector_data
+        The y-component of a vector quantity on the particles to
+        interpolate. This is the y-component with reference to the
+        coordinate system.
+    particle_mass
+        The particle mass on each particle.
+    x_range
+        The range in the x-direction as (xmin, xmax).
+    y_range
+        The vertical range as (ymin, ymax).
 
     Optional parameters
     -------------------
-    number_pixels : list of float (default [512, 512])
+    number_pixels
         The pixel grid to interpolate the scalar quantity to, as
-        [npixx, npixy].
-    cross_section : bool (default False)
+        (npixx, npixy).
+    density_weighted
+        Use density weighted interpolation.
+    cross_section
         Turn on cross section rendering.
-    slice_position : float (default 0.0)
+    slice_position
         Slice position as a z-value.
-    perspective : bool (default False)
+    perspective
         Turn on perspective rendering.
-    observer_distance : float (default 0.0)
+    observer_distance
         Distance from the screen to the observer. This turns on 3D
         perspective rendering.
-    normalize : bool
+    normalize
         Use normalized interpolation.
-    integrated_z : float (default None)
+    integrated_z
         Z unit for projection plots.
 
     Returns
     -------
-    numpy.ndarray
+    ndarray
         An array of vector quantities interpolated to a pixel grid with
         shape (2, npixx, npixy).
     """
-
-    if number_pixels is None:
-        npixx = 512
-        npixy = 512
-    else:
-        npixx = number_pixels[0]
-        npixy = number_pixels[1]
-
-    projection = False
-    if cross_section is None:
-        cross_section = False
-    if perspective is None:
-        perspective = False
-    if not cross_section and not perspective:
-        projection = True
-
-    if cross_section and slice_position is None:
-        slice_position = 0.0
+    npixx = number_pixels[0]
+    npixy = number_pixels[1]
+    projection = not cross_section and not perspective
     zslice = slice_position
-
-    if perspective and observer_distance is None:
-        observer_distance = 0.0
-
-    if observer_distance is not None:
-        dscreen = observer_distance
-        zobserver = observer_distance
-    else:
-        dscreen = 0.0
-        zobserver = 0.0
-
-    if normalize is None:
-        normalise = False
-    else:
-        normalise = normalize
-
+    dscreen = observer_distance
+    zobserver = observer_distance
+    normalise = normalize
     npart = len(smoothing_length)
-    itype = np.ones(npart, dtype=np.int32)
-
-    xmin = horizontal_range[0]
-    ymin = vertical_range[0]
-    xmax = horizontal_range[1]
-    ymax = vertical_range[1]
+    xmin = x_range[0]
+    ymin = y_range[0]
+    xmax = x_range[1]
+    ymax = y_range[1]
     pixwidthx = (xmax - xmin) / npixx
     pixwidthy = (ymax - ymin) / npixy
+    if density_weighted:
+        weights = particle_mass / smoothing_length ** 2
+    else:
+        weights = np.ones(npart) / smoothing_length_factor
 
     # Splash routines expect single precision
-    x = np.array(positions[:, 0], dtype=np.single)
-    y = np.array(positions[:, 1], dtype=np.single)
-    z = np.array(positions[:, 2], dtype=np.single)
+    x = np.array(x_coordinate, dtype=np.single)
+    y = np.array(y_coordinate, dtype=np.single)
+    z = np.array(z_coordinate, dtype=np.single)
     hh = np.array(smoothing_length, dtype=np.single)
-    vecx = np.array(vector_data[:, 0], dtype=np.single)
-    vecy = np.array(vector_data[:, 1], dtype=np.single)
+    vecx = np.array(x_vector_data, dtype=np.single)
+    vecy = np.array(y_vector_data, dtype=np.single)
     weight = np.array(weights, dtype=np.single)
+    itype = np.ones(npart, dtype=np.int32)
 
     if projection or perspective:
         vecsmoothx, vecsmoothy = splash.interpolate3d_proj_vec(
