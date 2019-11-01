4e7c8e56bbf1e5da141c7ceed1aeff29a9351c17
Daniel Mentiplay <d.mentiplay@gmail.com>
2019-11-01 16:29:29 +1100

    Add decorator to add profiles via functions

--- plonk/analysis/profile.py
+++ plonk/analysis/profile.py
@@ -4,8 +4,9 @@ Heavily inspired by pynbody (https://pynbody.github.io/).
 """
 
 from bisect import bisect
-from typing import Dict, List, Optional, Tuple
+from typing import Any, Callable, Collection, Dict, List, Optional, Tuple, Union
 
+import matplotlib.pyplot as plt
 import numpy as np
 from numpy import ndarray
 
@@ -37,6 +38,8 @@ class Profile:
         Select a subset of all particles via a NumPy mask array.
     """
 
+    _profile_functions: Dict[str, Callable] = {}
+
     def __init__(
         self,
         dump: Dump,
@@ -62,13 +65,9 @@ class Profile:
         self.bin_indicies = self._set_particle_bin_indicies()
 
         self._profiles: Dict[str, ndarray] = {}
-        self._profiles['radius'] = self.bin_centers
-        n_per_bin, _ = np.histogram(self._x, self.bin_edges)
-        self._profiles['number'] = n_per_bin
 
-        self._profiles['mass'] = mass(self)
-        self._profiles['density'] = density(self)
-        self._profiles['angular momentum'] = angular_momentum(self)
+        self._profiles['radius'] = self.bin_centers
+        self._profiles['number'] = np.histogram(self._x, self.bin_edges)[0]
 
     def _setup_particle_mask(
         self, ignore_accreted: bool, mask: Optional[ndarray]
@@ -92,11 +91,14 @@ class Profile:
     def _set_range(
         self, radius_min: Optional[float], radius_max: Optional[float]
     ) -> Tuple[float, float]:
-        rmin, rmax = (radius_min, radius_max)
         if radius_min is None:
             rmin = self._x.min()
+        else:
+            rmin = radius_min
         if radius_max is None:
             rmax = np.percentile(self._x, 99, axis=0)
+        else:
+            rmax = radius_max
 
         return rmin, rmax
 
@@ -119,12 +121,30 @@ class Profile:
             prev_index = new_index
         return binind
 
-    def __getitem__(self, name: str) -> ndarray:
-        """Return the profile of a given kind."""
+    def _get_profile(self, name: str, args: Optional[Tuple[Any, ...]] = None):
+        """Get a profile by name."""
         if name in self._profiles:
             return self._profiles[name]
+
+        elif name in Profile._profile_functions:
+            if args is not None:
+                self._profiles[name] = Profile._profile_functions[name](self, *args)
+            self._profiles[name] = Profile._profile_functions[name](self)
+            return self._profiles[name]
+
         else:
             raise ValueError('Profile not available')
+
+    def __getitem__(self, name: str) -> ndarray:
+        """Return the profile of a given kind."""
+        return self._get_profile(name)
+
+    def __setitem__(self, name: str, values: ndarray):
+        """Set the profile directly."""
+        if name in self._profiles:
+            self._profiles[name] = values
+        else:
+            raise KeyError(f'{name} is not a valid profile')
 
     def __delitem__(self, name):
         """Delete a profile from memory."""
@@ -132,16 +152,58 @@ class Profile:
 
     def __repr__(self):
         """Object repr method."""
-        return (
-            f'<plonk.Profile: {self.n_bins} bins; '
-            f'{self.range[0]:.3g} to {self.range[1]:.3g}>'
+        return f'<plonk.Profile: {self.n_bins} bins>'
+
+    def loaded_keys(self):
+        """Return a listing of loaded profiles."""
+        return tuple(sorted(self._profiles.keys()))
+
+    def available_keys(self):
+        """Return a listing of available profiles."""
+        return tuple(
+            sorted(
+                set(
+                    tuple(self._profile_functions.keys()) + tuple(self._profiles.keys())
+                )
+            )
         )
 
-    def keys(self):
-        """Return a listing of available profile types."""
-        return self._profiles.keys()
+    @staticmethod
+    def profile_property(fn):
+        """Decorate profile functions."""
+        Profile._profile_functions[fn.__name__] = fn
+        return fn
+
+    def plot(self, x: str, y: Union[str, Collection[str]]):
+        """Plot profile.
+
+        Parameters
+        ----------
+        x
+            The x axis to plot as a string.
+        y
+            The y axis to plot. Can be multiple as a list or tuple.
+        """
+        if x.lower() not in self.available_keys():
+            raise ValueError('Cannot determine x axis to plot')
+        _x = self._get_profile(x)
+        if isinstance(y, (list, tuple)):
+            for yi in y:
+                if yi.lower() not in self.available_keys():
+                    raise ValueError('Cannot determine y axis to plot')
+                _y = self._get_profile(yi)
+                plt.plot(_x, _y)
+        elif isinstance(y, str):
+            if y.lower() not in self.available_keys():
+                raise ValueError('Cannot determine y axis to plot')
+            _y = self._get_profile(y)
+            plt.plot(_x, _y)
+        else:
+            raise ValueError('Cannot determine y axis to plot')
+        return plt.gcf(), plt.gca()
 
 
+@Profile.profile_property
 def mass(profile: Profile) -> ndarray:
     """Mass profile."""
     mass = np.zeros(profile.n_bins)
@@ -151,25 +213,66 @@ def mass(profile: Profile) -> ndarray:
     return mass
 
 
+@Profile.profile_property
 def density(profile: Profile) -> ndarray:
     """Density profile.
 
     Units are [mass / length ** ndim], which depends on ndim of profile.
     """
-    return profile._profiles['mass'] / profile.bin_sizes
+    return profile._get_profile('mass') / profile.bin_sizes
 
 
-def angular_momentum(profile: Profile) -> ndarray:
-    """Angular momentum profile.
+@Profile.profile_property
+def angmom_mag(profile: Profile) -> ndarray:
+    """Magnitude of specific angular momentum profile."""
+    angmom_mag = np.zeros(profile.n_bins)
 
-    The angular momentum is with respect to the xy-plane.
-    """
     mass = profile.dump.mass[:, np.newaxis]
     pos = profile.dump.particles.arrays['xyz'][:]
     vel = profile.dump.particles.arrays['vxyz'][:]
-    L = mass * np.cross(pos, vel)
-    L_z = L[:, 2]
-    angular_momentum = np.zeros(profile.n_bins)
+
+    J = mass * np.cross(pos, vel)
+    J_mag = np.hypot(np.hypot(J[:, 0], J[:, 1]), J[:, 2])
+
     for idx, bin_ind in enumerate(profile.bin_indicies):
-        angular_momentum[idx] = L_z[bin_ind].mean()
-    return angular_momentum
+        angmom_mag[idx] = J_mag[bin_ind].mean() / profile._get_profile('mass')[idx]
+    return angmom_mag
+
+
+@Profile.profile_property
+def angmom_theta(profile: Profile) -> ndarray:
+    """Angle between angular momentum and xy-plane."""
+    angmom_theta = np.zeros(profile.n_bins)
+
+    mass = profile.dump.mass[:, np.newaxis]
+    pos = profile.dump.particles.arrays['xyz'][:]
+    vel = profile.dump.particles.arrays['vxyz'][:]
+
+    J = mass * np.cross(pos, vel)
+    J_z = J[:, 2]
+    J_mag = np.hypot(np.hypot(J[:, 0], J[:, 1]), J[:, 2])
+
+    for idx, bin_ind in enumerate(profile.bin_indicies):
+        j_z = J_z[bin_ind].mean() / profile._get_profile('mass')[idx]
+        j_mag = J_mag[bin_ind].mean() / profile._get_profile('mass')[idx]
+        angmom_theta[idx] = np.arccos(j_z / j_mag)
+    return angmom_theta
+
+
+@Profile.profile_property
+def angmom_phi(profile: Profile) -> ndarray:
+    """Angle between angular momentum and x-axis in xy-plane."""
+    angmom_phi = np.zeros(profile.n_bins)
+
+    mass = profile.dump.mass[:, np.newaxis]
+    pos = profile.dump.particles.arrays['xyz'][:]
+    vel = profile.dump.particles.arrays['vxyz'][:]
+
+    J = mass * np.cross(pos, vel)
+    J_x, J_y = (J[:, 0], J[:, 1])
+
+    for idx, bin_ind in enumerate(profile.bin_indicies):
+        j_x = J_x[bin_ind].mean() / profile._get_profile('mass')[idx]
+        j_y = J_y[bin_ind].mean() / profile._get_profile('mass')[idx]
+        angmom_phi[idx] = np.arctan2(j_y, j_x)
+    return angmom_phi
