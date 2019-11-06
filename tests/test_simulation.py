7cb431b05825c9c1b58fee14e182d1277acec94b
Daniel Mentiplay <d.mentiplay@gmail.com>
2019-11-06 16:11:16 +1100

    Fix test failures in simulation and evolution

--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -1,29 +1,18 @@
-"""
-Testing Simulation.
-"""
+"""Testing Simulation."""
 
 import pathlib
-import unittest
 
 import plonk
 
 
-class TestReadPhantomSimulation(unittest.TestCase):
-    """Test reading Phantom simultion data."""
+def test_init_simulation():
+    """Testing initialising simulation."""
+    dir_path = pathlib.Path(__file__).parent / 'stubdata'
+    sim = plonk.load_sim(prefix='phantom', directory=dir_path)
 
-    def test_init_simulation(self):
-        """Testing initialising simulation."""
+    snaps = sim.snaps
+    assert len(snaps) == 1
+    assert len(snaps[0]) == 2000
 
-        dir_path = pathlib.Path(__file__).parent / 'stubdata'
-        sim = plonk.Simulation(prefix='phantom', directory=dir_path)
-
-        dumps = sim.dumps
-        self.assertEqual(len(dumps), 1)
-        self.assertEqual(dumps[0].file_name, 'phantom_00000.h5')
-
-        ev = sim.evolution
-        self.assertEqual(ev.file_names[0], 'phantom01.ev')
-
-
-if __name__ == '__main__':
-    unittest.main(verbosity=2)
+    ev = sim.global_quantities
+    assert ev.file_names[0] == 'phantom01.ev'
