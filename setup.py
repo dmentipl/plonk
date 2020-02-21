98b59cf78ddda0f1b14a4ef82979a3814074a4ea
Daniel Mentiplay <d.mentiplay@gmail.com>
2020-02-21 15:08:11 +1100

    Add Python 3.8 PyPI classifier to setup.py

--- setup.py
+++ setup.py
@@ -51,6 +51,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Operating System :: OS Independent",
         "Topic :: Scientific/Engineering :: Astronomy",
         "Topic :: Scientific/Engineering :: Visualization",
