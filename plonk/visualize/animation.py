af1520894334c4b49b418342d620ebc34be4721b
Daniel Mentiplay <d.mentiplay@gmail.com>
2020-03-09 22:58:52 +1100

    Pass kwargs to visualize.animation

--- plonk/visualize/animation.py
+++ plonk/visualize/animation.py
@@ -2,7 +2,7 @@
 
 import pathlib
 from pathlib import Path
-from typing import List, Optional, Tuple, Union
+from typing import Any, Dict, List, Optional, Tuple, Union
 
 import matplotlib.pyplot as plt
 from matplotlib import animation as _animation
@@ -22,14 +22,8 @@ def animation(
     filename: Union[str, Path],
     snaps: List[SnapLike],
     data: Optional[Union[str, ndarray]] = None,
-    x: Union[str, ndarray] = 'x',
-    y: Union[str, ndarray] = 'y',
-    z: Union[str, ndarray] = 'z',
-    kind: Optional[str] = None,
-    interp: str = 'projection',
-    z_slice: float = 0.0,
     extent: Tuple[float, float, float, float],
-    animation_kwargs,
+    animation_kwargs: Dict[str, Any] = {},
     **kwargs,
 ):
     """Generate an animation.
@@ -44,81 +38,35 @@ def animation(
         The data to visualize. Can be a string to pass to Snap, or
         a 1d array (N,) of scalar data, or a 2d array (N, 3) of
         vector data. Default is None.
-    x
-        The x-coordinate for the visualization. Can be a string to
-        pass to Snap, or a 1d array (N,). Default is 'x'.
-    y
-        The y-coordinate for the visualization. Can be a string to
-        pass to Snap, or a 1d array (N,). Default is 'y'.
-    z
-        The z-coordinate for the visualization. Can be a string to
-        pass to Snap, or a 1d array (N,). This is only required for
-        cross-section plots. Default is 'z'.
-    kind
-        The type of plot.
-        - 'particle' : particle plot (default if data is None)
-        - 'render' : rendered image (default for scalar data)
-        - 'contour' : contour plot (scalar data)
-        - 'quiver' : quiver (arrow) plot (default for vector data)
-        - 'stream' : stream plot (vector data)
-    interp
-        The interpolation type.
-        - 'projection' : 2d interpolation via projection to xy-plane
-        - 'cross_section' : 3d interpolation via cross-section in
-            z-direction
-        Default is 'projection'.
-    z_slice
-        The z-coordinate value of the cross-section slice. Default
-        is 0.0.
     extent
-        The extent of the image like (xmin, xmax, ymin, ymax).
+        The range in the x and y-coord as (xmin, xmax, ymin, ymax).
     animation_kwargs : optional
         Key word arguments to pass to matplotlib Animation.save.
+    **kwargs
+        Arguments to pass to visualize.plot.
     """
     filepath = pathlib.Path(filename)
     if filepath.suffix != '.mp4':
         raise ValueError('filename should end in ".mp4"')
 
     fig, ax = plt.subplots()
-    viz = plot(
-        snap=snaps[0],
-        data=data,
-        x=x,
-        y=y,
-        z=z,
-        kind=kind,
-        interp=interp,
-        z_slice=z_slice,
-        extent=extent,
-        **kwargs,
-    )
-    scalar_data = viz.data['scalar']
-    im = ax.imshow(scalar_data)
+    viz = plot(snap=snaps[0], data=data, extent=extent, **kwargs)
+    interp_data = _get_data(viz.data)
+    im = ax.imshow(interp_data)
 
     # If plotting colorbar, must fix the render range
     vmin, vmax = None, None
     show_colorbar = kwargs.pop('show_colorbar', True)
     if show_colorbar is None or show_colorbar is True:
-        vmin, vmax = (scalar_data.min(), scalar_data.max())
+        vmin, vmax = (interp_data.min(), interp_data.max())
 
     def animate(idx):
         print(f'Rendering image: {idx}')
         viz = plot(
-            snap=snaps[idx],
-            data=data,
-            x=x,
-            y=y,
-            z=z,
-            kind=kind,
-            interp=interp,
-            z_slice=z_slice,
-            extent=extent,
-            vmin=vmin,
-            vmax=vmax,
-            **kwargs,
+            snap=snaps[idx], data=data, extent=extent, vmin=vmin, vmax=vmax, **kwargs
         )
-        scalar_data = viz.data['scalar']
-        im.set_data(scalar_data)
+        interp_data = _get_data(viz.data)
+        im.set_data(interp_data)
         return [im]
 
     anim = _animation.FuncAnimation(fig, animate, frames=len(snaps))
@@ -126,3 +74,9 @@ def animation(
         filepath, extra_args=['-vcodec', 'libx264'], **animation_kwargs,
     )
     plt.close()
+
+
+def _get_data(data):
+    for k, v in data.items():
+        if v is not None:
+            return v
