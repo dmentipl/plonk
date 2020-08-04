Fix axis and cbar label units in plot

plonk/visualize/visualization.py
@@ -6,13 +6,15 @@ from copy import copy
 from typing import TYPE_CHECKING, Any, Dict, Optional
 
 import matplotlib.pyplot as plt
+import numpy as np
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from numpy import ndarray
 
 from .._logging import logger
 from .._units import Quantity
+from .._units import units as plonk_units
 from . import plots
-from .functions import get_extent_from_percentile, str_to_units
+from .functions import get_extent_from_percentile
 from .interpolation import Extent, interpolate
 
 if TYPE_CHECKING:
@@ -168,15 +170,16 @@ def plot(
             'projection': 1 * snap['position'].units,
         }
     else:
-        qunit = units.get('quantity', str(snap[quantity].units))
-        eunit = units.get('extent', str(snap['position'].units))
-        punit = units.get('projection', str(snap['position'].units))
-        _units = str_to_units(quantity=qunit, extent=eunit, projection=punit)
+        qunit = 1 * plonk_units(units.get('quantity', str(snap[quantity].units)))
+        eunit = 1 * plonk_units(units.get('extent', str(snap['position'].units)))
+        punit = 1 * plonk_units(units.get('projection', str(snap['position'].units)))
+        _units = {'quantity': qunit, 'extent': eunit, 'projection': punit}
 
     interpolation_kwargs = ('number_of_pixels', 'density_weighted')
     __kwargs = {key: val for key, val in _kwargs.items() if key in interpolation_kwargs}
     for key in __kwargs:
         _kwargs.pop(key)
+
     # Interpolate in code units
     interpolated_data = interpolate(
         snap=snap,
@@ -225,15 +228,21 @@ def plot(
         cax = divider.append_axes(position=position, size=size, pad=pad)
         cbar = fig.colorbar(plot_object, cax, **_kwargs)
         if interp == 'projection':
-            qunit = _units['quantity'].units * _units['projection'].units
+            qunit = _units['quantity'] * _units['projection']
         elif interp == 'cross_section':
-            qunit = _units['quantity'].units
+            qunit = _units['quantity']
+        if np.allclose(qunit.magnitude, 1.0):
+            qunit = qunit.units
         cbar.set_label(f'{quantity} [{qunit:~P}]')
 
     ax.set_xlim(*extent[:2])
     ax.set_ylim(*extent[2:])
-    ax.set_xlabel(f'{x} [{_units["extent"].units:~P}]')
-    ax.set_ylabel(f'{y} [{_units["extent"].units:~P}]')
+
+    eunit = _units['extent']
+    if np.allclose(eunit.magnitude, 1.0):
+        eunit = eunit.units
+    ax.set_xlabel(f'{x} [{eunit:~P}]')
+    ax.set_ylabel(f'{y} [{eunit:~P}]')
 
     ratio = (extent[1] - extent[0]) / (extent[3] - extent[2])
     if not max(ratio, 1 / ratio) > 10.0:
@@ -249,9 +258,7 @@ def particle_plot(
     y: str = 'y',
     c: Optional[str] = None,
     s: Optional[str] = None,
-    xunit: Any = None,
-    yunit: Any = None,
-    cunit: Any = None,
+    units: Dict[str, str] = None,
     xscale: str = None,
     yscale: str = None,
     ax: Optional[Any] = None,
@@ -279,12 +286,10 @@ def particle_plot(
     s
         The quantity to set the particle size. Must be a string to
         pass to Snap.
-    xunit
-        The units of the x-coordinate.
-    yunit
-        The units of the y-coordinate.
-    cunit
-        The units of the color.
+    units
+        The units of the plot as a dictionary with keys 'x', 'y', 'c',
+        and 's'. The values are strings representing units, e.g.
+        'g/cm^3'.
     xscale
         The xscale to pass to the matplotlib Axes method set_xscale.
     yscale
@@ -312,9 +317,7 @@ def particle_plot(
         'y': y,
         'c': c,
         's': s,
-        'xunit': xunit,
-        'yunit': yunit,
-        'cunit': cunit,
+        'units': units,
         'xscale': xscale,
         'yscale': yscale,
         'fig': fig,
@@ -338,9 +341,7 @@ def _particle_plot(
     y='y',
     c=None,
     s=None,
-    xunit=None,
-    yunit=None,
-    cunit=None,
+    units=None,
     xscale=None,
     yscale=None,
     fig,
@@ -355,20 +356,37 @@ def _particle_plot(
     _c: ndarray = snap[c] if c is not None else None
     _s: ndarray = snap[s] if s is not None else None
 
-    if xunit is not None:
-        _x = _x.to(xunit).magnitude
+    if units is None:
+        _units = {
+            'x': 1 * snap[x].units,
+            'y': 1 * snap[y].units,
+        }
+        if c is not None:
+            _units['c'] = 1 * snap[c].units
+        if s is not None:
+            _units['s'] = 1 * snap[s].units
     else:
-        _x = _x.magnitude
-    if yunit is not None:
-        _y = _y.to(yunit).magnitude
-    else:
-        _y = _y.magnitude
-    if cunit is not None:
-        _c = _c.to(cunit).magnitude
-    else:
-        _c = _c.magnitude
+        xunit = units.get('x', str(snap[x].units))
+        yunit = units.get('y', str(snap[y].units))
+        _units = {
+            'x': 1 * plonk_units(xunit),
+            'y': 1 * plonk_units(yunit),
+        }
+        if c is not None:
+            cunit = units.get('c', str(snap[c].units))
+            _units['c'] = 1 * plonk_units(cunit)
+        if s is not None:
+            sunit = units.get('s', str(snap[s].units))
+            _units['s'] = 1 * plonk_units(sunit)
 
-    h: ndarray = snap['smoothing_length']
+    _x = _x.to(_units['x']).magnitude
+    _y = _y.to(_units['y']).magnitude
+    if _c is not None:
+        _c = _c.to(_units['c']).magnitude
+    if _s is not None:
+        _s = _s.to(_units['s']).magnitude
+
+    h: ndarray = snap['smoothing_length'].m
     mask = h > 0
 
     _x = _x[mask]
@@ -396,7 +414,11 @@ def _particle_plot(
             if position in ('top', 'bottom'):
                 _kwargs.update({'orientation': 'horizontal'})
             cax = divider.append_axes(position=position, size=size, pad=pad)
-            fig.colorbar(plot_object, cax, **_kwargs)
+            cbar = fig.colorbar(plot_object, cax, **_kwargs)
+            cunit = _units['c']
+            if np.allclose(cunit.magnitude, 1.0):
+                cunit = cunit.units
+            cbar.set_label(f'{c} [{cunit:~P}]')
 
     if xscale is not None:
         ax.set_xscale(xscale)
@@ -406,6 +428,14 @@ def _particle_plot(
     ratio = (_x.max() - _x.min()) / (_y.max() - _y.min())
     if not max(ratio, 1 / ratio) > 10.0:
         ax.set_aspect('equal')
+
+    xunit, yunit = _units['x'], _units['y']
+    if np.allclose(xunit.magnitude, 1.0):
+        xunit = xunit.units
+    if np.allclose(yunit.magnitude, 1.0):
+        yunit = yunit.units
+    ax.set_xlabel(f'{x} [{xunit:~P}]')
+    ax.set_ylabel(f'{y} [{yunit:~P}]')
 
 
 def _convert_units(
@@ -420,16 +450,20 @@ def _convert_units(
     required_keys = {'extent', 'projection', 'quantity'}
     if not set(units) == required_keys:
         raise ValueError(f'units dictionary requires: {required_keys}')
-    _quantity_unit = snap.get_array_unit(quantity)
+    quantity_unit = snap.get_array_unit(quantity)
     if interp == 'projection':
-        data = (
-            (interpolated_data * _quantity_unit * snap.units['length'])
-            .to(units['quantity'] * units['projection'])
-            .magnitude
-        )
+        proj_unit = units['quantity'] * units['projection']
+        data = (interpolated_data * quantity_unit * snap.units['length']).to(
+            proj_unit.units
+        ).magnitude / proj_unit.magnitude
     elif interp == 'cross_section':
-        data = (interpolated_data * _quantity_unit).to(units['quantity']).magnitude
+        data = (interpolated_data * quantity_unit).to(
+            units['quantity'].units
+        ).magnitude / units['quantity'].magnitude
 
-    new_extent = tuple((extent * snap.units['length']).to(units['extent']).magnitude)
+    new_extent = tuple(
+        (extent * snap.units['length']).to(units['extent'].units).magnitude
+        / units['extent'].magnitude
+    )
 
     return data, new_extent