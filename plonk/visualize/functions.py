f58c18b1d57ea1683c6f6b1b1f8f9ba82cc9bef4
Daniel Mentiplay <d.mentiplay@gmail.com>
2020-08-11 22:37:53 +1000

    Update plot_smoothing_length function

--- plonk/visualize/functions.py
+++ plonk/visualize/functions.py
@@ -2,10 +2,13 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, List
+from typing import TYPE_CHECKING, Any, List, Union
 
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.collections import PatchCollection
+from matplotlib.patches import Circle
+
 
 from .._units import Quantity
 
@@ -17,13 +20,15 @@ def plot_smoothing_length(
     snap: SnapLike,
     indices: List[int],
     fac: float = 2.0,
-    units: Quantity = None,
+    units: Union[str, Quantity] = None,
     x: str = 'x',
     y: str = 'y',
     ax: Any = None,
     **kwargs
 ) -> Any:
     """Plot smoothing length around particle.
+
+    Also works for plotting the accretion radius on a sink particle.
 
     Parameters
     ----------
@@ -42,30 +47,35 @@ def plot_smoothing_length(
     y
         The "y" coordinate.
     **kwargs
-        Keyword arguments to pass to plt.Circle.
+        Keyword arguments to pass to matplotlib PatchCollection.
 
     Returns
     -------
     circles
         A list of matplotlib circles.
     """
-    if ax is None:
-        fig, ax = plt.subplots()
+    px: Quantity = snap[x][indices]
+    py: Quantity = snap[y][indices]
+    try:
+        h: Quantity = snap['smoothing_length'][indices]
+    except ValueError:
+        # Sinks accretion radius is equivalent to smoothing length
+        h = snap['accretion_radius'][indices]
     if units is None:
         units = snap[x].units
-    circles = list()
-    for index in indices:
-        px: Quantity = snap[x][index]
-        py: Quantity = snap[y][index]
-        h: Quantity = snap['smoothing_length'][index]
-        px = px.to(units).magnitude
-        py = py.to(units).magnitude
-        h = h.to(units).magnitude
-        pos = (px, py)
-        circles.append(plt.Circle(pos, fac * h, **kwargs))
-    for c in circles:
-        ax.add_artist(c)
-    return circles
+    px = px.to(units).magnitude
+    py = py.to(units).magnitude
+    h = h.to(units).magnitude
+
+    circles = [Circle((_px, _py), fac * _h) for _px, _py, _h in zip(px, py, h)]
+    collection = PatchCollection(circles, **kwargs)
+    if ax is None:
+        fig, ax = plt.subplots()
+        ax.set_aspect('equal')
+    ax.add_collection(collection)
+    _set_new_lim(ax, px, py, h, fac)
+
+    return collection
 
 
 def get_extent_from_percentile(
@@ -119,3 +129,15 @@ def get_extent_from_percentile(
         return (xlim[0], xlim[1], ylim[0], ylim[1])
 
     return (xlim[0], xlim[1], ylim[0], ylim[1])
+
+
+def _set_new_lim(ax, x, y, h, fac):
+    xlim, ylim = ax.get_xlim(), ax.get_ylim()
+    x_min = x.min() - fac * h.max()
+    x_max = x.max() + fac * h.max()
+    y_min = y.min() - fac * h.max()
+    y_max = y.max() + fac * h.max()
+    _xlim = (min(x_min, xlim[0]), max(x_max, xlim[1]))
+    _ylim = (min(y_min, ylim[0]), max(y_max, ylim[1]))
+    ax.set_xlim(_xlim)
+    ax.set_ylim(_ylim)
