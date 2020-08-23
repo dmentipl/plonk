fb6103d987c2f2f6044364a91847b7176a02e0bd
Daniel Mentiplay <d.mentiplay@gmail.com>
2020-08-23 15:12:22 +1000

    Add function name to logging messages

--- plonk/_logging.py
+++ plonk/_logging.py
@@ -14,7 +14,9 @@ console_handler.setLevel(logging.INFO)
 file_handler.setLevel(logging.DEBUG)
 
 console_format = logging.Formatter('%(levelname)s - %(message)s')
-file_format = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+file_format = logging.Formatter(
+    '%(asctime)s - %(levelname)s - %(funcName)s - %(message)s'
+)
 console_handler.setFormatter(console_format)
 file_handler.setFormatter(file_format)
 
